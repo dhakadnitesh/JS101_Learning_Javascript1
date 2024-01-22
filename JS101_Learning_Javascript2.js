9e8499dd70da1656ef3ab81e76d834de0335e250
dhakadnitesh <niteshdhakad837@gmail.com>
2024-01-22 10:39:26 +0530

    location2

---
+++ JS101_Learning_Javascript2.js
@@ -0,0 +1,27 @@
+let a=4;
+let b=7;
+
+console.log("The sum is :-", a+b);
+console.log("The Difference is :-", a-b);
+console.log("The Multiplication is :-", a*b);
+console.log("The Division is :-", a/b);
+console.log("The moduals is :-",a%b)
+
+
+let c=2;
+let d=3;
+
+console.log(c**d); 
+
+let x=2;
+let y=2;
+let z=x*y;
+
+console.log(z); 
+
+let e="Hello";
+let f="World";
+
+
+
+console.log(e+" "+f) 
