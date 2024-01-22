6022568af39d725b01f4c015fcd19c24f7ceea53
dhakadnitesh <niteshdhakad837@gmail.com>
2024-01-22 10:19:09 +0530

    location 1

---
+++ JS101_Learning_Javascript1.js
@@ -0,0 +1,19 @@
+let a;          
+a = 10;         
+console.log(a);
+
+let b = 10;
+console.log(b)
+
+let name = "gohan";
+console.log(name);
+
+let x = 12;
+console.log(typeof(x)); 
+
+let y = "12";
+console.log(typeof(y));
+
+var c = "ram"
+var d = "sharma"
+console.log(c+" "+d)
